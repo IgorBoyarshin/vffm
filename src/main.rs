d31753d8bc5840fcbaf6619cecbde8cda34a75f2
Igor Boyarshin <igor.boyarshin@gmail.com>
2019-01-09 19:36:21 +0200

    Changed to another lib

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
-extern crate termion;
+extern crate pancurses;
+
+use pancurses::{initscr, endwin, Input, noecho};
 
 fn main() {
-    println!("Hello, world!");
+    let window = initscr();
+    window.printw("Type things, press delete to quit\n");
+    window.refresh();
+    window.keypad(true);
+    noecho();
+    loop {
+        match window.getch() {
+            Some(Input::Character(c)) => { window.addch(c); window.refresh(); },
+            Some(Input::KeyDC) => break,
+            // Some(input) => { window.addstr(&format!("{:?}", input)); },
+            _ => ()
+        }
+    }
+    endwin();
 }
