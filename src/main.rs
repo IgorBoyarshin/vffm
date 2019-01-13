2011e9c80e48434d0db6421d5747c7e225e65b24
Igor Boyarshin <igor.boyarshin@gmail.com>
2019-01-13 18:01:08 +0200

    Has current path

--- src/main.rs
+++ src/main.rs
@@ -8,21 +8,28 @@ use crate::system::*;
 mod filesystem;
 use crate::filesystem::*;
 
+// use std::path::Path;
+// use std::fs::{self};
 
 fn main() {
     let mut color_system = ColorSystem::new();
+    let mut starting_path = absolute_pathbuf();
+    starting_path.pop();
+    starting_path.pop();
+    starting_path.pop();
     let system = System::new(
         Settings {
-            columns_ratio: vec![1,3,1],
+            columns_ratio: vec![2,3,2],
             dir_paint: Paint {fg: Color::Cyan, bg: Color::Black, bold: true, underlined: false},
             symlink_paint: Paint {fg: Color::Yellow, bg: Color::Black, bold: true, underlined: false},
             file_paint: Paint {fg: Color::White, bg: Color::Black, bold: false, underlined: false},
-        }
+        },
+        starting_path,
     );
     system.clear(&mut color_system);
     system.draw(&mut color_system);
 
     // let names = collect_dir(".").into_iter().map(|f| f.name).collect();
-    system.list_entries(&mut color_system, 1, collect_dir("."));
+    // system.list_entries(&mut color_system, 1, collect_dir("."));
     system.get();
 }
