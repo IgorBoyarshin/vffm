0327bd83a54179de5ee090d280d826f6885a40f5
Igor Boyarshin <igor.boyarshin@gmail.com>
2019-01-20 13:21:17 +0200

    Implemented input system

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::time::Duration;
 use std::thread;
+// use std::collections::HashMap;
 
 extern crate pancurses;
 use pancurses::Input;
@@ -16,13 +17,67 @@ use crate::filesystem::*;
 // use std::path::Path;
 // use std::fs::{self};
 
+
+#[derive(PartialEq, Eq)]
+enum Mode {
+    Input,
+    AwaitingCommand,
+}
+
+type Combination = String;
+enum Command {
+    Terminate,
+    GoTo(String),
+    Up,
+    Down,
+    Left,
+    Right,
+    // NewTab,
+    // CloseTab,
+    // NextTab,
+    // PreviousTab,
+}
+
+fn generate_possible_inputs() -> Vec<(Combination, Command)> {
+    let mut inputs = Vec::new();
+    inputs.push(("qwq".to_string(), Command::Terminate));
+    inputs.push(("hh".to_string(), Command::Left));
+    inputs.push(("j".to_string(), Command::Down));
+    inputs.push(("k".to_string(), Command::Up));
+    inputs.push(("l".to_string(), Command::Right));
+    inputs
+}
+
+fn vec_of_refs<'a, T>(array: &'a Vec<T>) -> Vec<&'a T> {
+    let mut vec = Vec::new();
+    for entry in array { vec.push(entry); }
+    vec
+}
+
+fn combinations_that_start_with<'a>(slice: &str, array: Vec<&'a(Combination, Command)>)
+        -> Vec<&'a (Combination, Command)> {
+    let mut combinations = Vec::new();
+    let str1 = slice.as_bytes();
+    let size = slice.len();
+    'entries: for entry in array {
+        let str2 = (&entry.0).as_bytes();
+        if size > str2.len() { continue 'entries; }
+        for i in 0..size {
+            if str1[i] != str2[i] { continue 'entries; }
+        }
+        // Complete match up to the size => take it
+        combinations.push(entry);
+    }
+    combinations
+}
+
+
 fn main() {
     let mut color_system = ColorSystem::new();
     let mut starting_path = absolute_pathbuf();
     starting_path.pop();
     starting_path.pop();
     starting_path.pop();
-    // println!("{:?}", starting_path);
     let mut system = System::new(
         Settings {
             columns_ratio: vec![2,3,2],
@@ -35,26 +90,45 @@ fn main() {
         starting_path,
     );
 
-    system.clear(&mut color_system);
-    system.draw(&mut color_system);
+    let current_mode = Mode::AwaitingCommand;
+    let possible_inputs = generate_possible_inputs();
+    let mut current_input = String::new();
+    let mut found_matches = vec_of_refs(&possible_inputs);
+    let exact_match = |found_matches: &Vec<&(Combination, Command)>, current_input: &str| {
+        (found_matches.len() == 1) &&
+            (found_matches[0].0.len() == current_input.len())
+    };
 
     let mut terminated = false;
     while !terminated {
-        if let Some(Input::Character(c)) = system.get() {
-            if c == 'q' {
-                terminated = true;
-            } else if c == 'j' {
-                system.down();
-            } else if c == 'k' {
-                system.up();
-            } else if c == 'h' {
-                system.left();
-            } else if c == 'l' {
-                system.right();
-            }
-        }
         system.clear(&mut color_system);
         system.draw(&mut color_system);
+
+        if let Some(Input::Character(c)) = system.get() {
+            if current_mode == Mode::AwaitingCommand {
+                current_input.push(c);
+                found_matches = combinations_that_start_with(&current_input, found_matches);
+                // Display matches
+                if exact_match(&found_matches, &current_input) {
+                    let (_, command) = found_matches.pop().unwrap();
+                    match command {
+                        Command::Terminate => terminated = true,
+                        Command::Up => system.up(),
+                        Command::Down => system.down(),
+                        Command::Left => system.left(),
+                        Command::Right => system.right(),
+                        Command::GoTo(_path) => {},
+                    }
+                }
+
+                if found_matches.len() == 0 { // done with current command
+                    // Reset for future commands
+                    current_input.clear();
+                    found_matches = vec_of_refs(&possible_inputs);
+                }
+            } else if current_mode == Mode::Input {}
+        }
+
         thread::sleep_ms(10);
     };
 }
